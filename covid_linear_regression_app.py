6dbf4049f400764b26fab433f4471609021c4d2b
linolanre <linolanre@gmail.com>
2023-05-15 15:55:05 +0100

    third_commit

--- covid_linear_regression_app.py
+++ covid_linear_regression_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+
 
 st.title('Predicting Future COVID-19 Cases')
 st.image('covidre.png')
 st.write('Enter a future date to predict the number of positive COVID-19 cases:')
 date = st.date_input('Date')
 st.button('Submit')
-import pickle
-regressor = pickle.load(open('reg.pkl', 'rb'))
+import joblib
+regressor = joblib.load(open('reg.pkl', 'rb'))
 
 prediction = regressor.predict(np.array(24/5/2024).reshape(-1,1))[0]
 st.write('Predicted number of positive cases:', prediction)
